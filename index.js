bdcc548e431470d59e66eeeeebbd1f5863be707b
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-02-23 19:40:47 +0100

    2

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function(request, response){
-			if (request.method === 'GET' && request.url === '/') {
-					fs.readFile('./index.html', 'utf-8', function(err, data) {
-						 response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-						if (!err) response.write(data);
-						response.end();
-					});
+            if (request.method === 'GET' && request.url === '/') {
+            fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            if (!err) response.write(data);
+            response.end();
+     });
 
-		} else {
+            } else {
 			fs.readFile('./404.jpg', function(err,data){
 			response.statusCode = 404;
 			if(!err) response.write(data);
 			response.end();
-		});
-		}
+    });
+    }
 });
 
 
